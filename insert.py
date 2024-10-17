9431fb37d2b46b103183ea964686455ef7e70837
rrr-cosa <rrr2505@outlook.com>
2024-10-17 11:16:11 -0600

    insert file example

---
+++ insert.py
@@ -0,0 +1,19 @@
+a = ["Master","Online"]
+b = "Code"
+a.insert(1, "Code")
+print(a)
+c = [1, 2, 3]
+c = [4, 5, 6]
+d = [1, 2, 3]
+c.insert(0, d)
+print(c)
+
+Lobos = ["Joseph","Hyrum","Alan","Rogelio","Rafa","Levi","Abinadi","Moroni","Nico","Benja","Angel"]
+Lobos.sort()
+print(Lobos)
+
+Lobos = ["Joseph","Hyrum","Alan","Rogelio","Rafa","Levi","Abinadi","Moroni","Nico","Benja","Angel"]
+Lobos.sort(reverse = True, key=len)
+print(Lobos)
+
+GOATS = ["Buffon","Roberto Carlos","Maldini","Beckenbauer","Cafu","Maradona","Zidane","Pele","Messi","Cristiano Ronaldo",""]
